cf0f0dd4e76d3cae0dcbe76cb9bbb2ce4a16b46a
AntonGerasimov <antoshkasg@gmail.com>
2015-05-14 01:47:35 +0300

    Add orient to ray.h

--- 051315/ray.h
+++ 051315/ray.h
@@ -17,6 +17,28 @@ inline float GradToRad(float deg) { return (float)(deg*PI/180.0);};
 inline float RadToGrad(float arg) { return (float)(arg*180.0/PI);};
 inline float Sqr(float x) {return x*x;}
 
+int orient (float x1, float y1, float x2, float y2, float x3, float y3, float this_deg) {// x1,y1; x2,y2 - line   x3,y3 - point
+    // cout << "x1 = " << x1 << " y1 = " << y1 << " x2 = " << x2 << " y2 = " << y2 << " x3 = " << x1 << " y3 = " << y3 << "\n";
+    if (this_deg == 0){
+        if (y3 <= y2){
+            
+            // cout << "trying 1 \n";
+            return 1;
+        }
+        else{
+            // cout << "trying -1 \n";
+            return -1;
+        }
+    }
+    else {
+        if (((x3 -x1) * (y2 - y1) - (y3 -y1) * (x2 - x1)) >= 0)
+            return 1;
+        else
+            return -1;
+    }
+    
+}
+
 /*
 class RAY{
 public:
@@ -97,9 +119,52 @@ public:
 };
 
 
-
-
 class SCREEN
+{
+    public :
+    float x1, x2, y1, y2,deg;
+public:
+    SCREEN (float x, float y, float l_0, float deg_0) {
+        x1 = x - (float)(l_0/2) * sin (deg_0 * PI / 180);
+        x2 = x + (float)(l_0/2) * sin (deg_0 * PI / 180);
+        y1 = y - (float)(l_0/2) * cos (deg_0 * PI / 180);
+        y2 = y + (float)(l_0/2) * cos (deg_0 * PI / 180);
+        deg = deg_0;
+    }
+    void screen_pos()
+    { cout << " x1 = " << x1 << " y1 = " << y1  << " x2 = " << x2 << " y2 = " << y2  <<"\n";}
+    
+    point * cross_point (RAY * r)  {
+        point * p = new point  ();
+        if ((orient (this->x1,this->y1,this->x2,this->y2,r->x,r->y,1) < 0) && ((r->deg <= 90) || (r->deg >= 270))){
+            float det = this->y2 - this->y1 - tan (r->deg * PI / 180) * (this->x1 - this->x2);
+            float det_1 = this->y2 * this->x1 - this->y1 * this->x2 - (this->x1 - this->x2) * (r->y + tan (r->deg * PI / 180) * r->x);
+            float det_2 = (this->y2 - this->y1) * (r->y + tan (r->deg * PI / 180) * r->x) - (this->y2 * this->x1 - this->y1 * this->x2) * tan (r->deg * PI / 180);
+            p->x = (float)det_1/det;
+            p->y = (float)det_2/det;
+            if (( p->x >= x1 ) && ( p->x <= x2 ) && ( p->y >= y1 ) && ( p->y <= y2 ))
+                return p;
+        }
+        if ((orient (this->x1,this->y1,this->x2,this->y2,r->x,r->y,1) > 0) && ((r->deg >= 90) && (r->deg <= 270))) {
+            float det = this->y2 - this->y1 - tan (r->deg * PI / 180) * (this->x1 - this->x2);
+            float det_1 = this->y2 * this->x1 - this->y1 * this->x2 - (this->x1 - this->x2) * (r->y + tan (r->deg * PI / 180) * r->x);
+            float det_2 = (this->y2 - this->y1) * (r->y + tan (r->deg * PI / 180) * r->x) - (this->y2 * this->x1 - this->y1 * this->x2) * tan (r->deg * PI / 180);
+            p->x = (float)det_1/det;
+            cout << "p->x = " << p->x << "\n" ;
+            p->y = (float)det_2/det;
+            cout << "p->x = " << p->x << "\n" ;
+            if (( p->x >= x1 ) && ( p->x <= x2 ) && ( p->y >= y1 ) && ( p->y <= y2 ))
+                return p;
+        }        return NULL;
+    }
+    
+    ~SCREEN ()
+    {
+        cout << "Destructure of the screen" << "\n";
+    }
+};
+
+/*class SCREEN
 {
 public :
     	float x1, x2, y1, y2;
@@ -137,7 +202,7 @@ public :
         	cout << "Destructure of the screen" << "\n";
     	}
 };
-
+*/
 class SOURCE
 {
 public:
