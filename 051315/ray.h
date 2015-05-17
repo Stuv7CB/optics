9da1208f25c6fade679667c857b40fee96dd1710
AntonGerasimov <antoshkasg@gmail.com>
2015-05-17 20:56:17 +0300

    Update ray.h

--- 051315/ray.h
+++ 051315/ray.h
@@ -8,7 +8,6 @@ using namespace std;
 #define PI 3.14159265
 #define NUMBER 37            // HERE YOU CAN CHANGE NUMBER OF CREATED RAYS {360 : (NUMBBER - 1)}
 
-
 struct point  {
     float x ;
     float y ;
